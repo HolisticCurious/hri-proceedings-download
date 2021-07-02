f909aef6fbacb84f9b4b65af25a19649f4487b17
MTeresaParreira <62056130+HolisticCurious@users.noreply.github.com>
2021-07-02 21:56:53 +0200

    Update retrieve_titles_urls_from_websites.py

--- retrieve_titles_urls_from_websites.py
+++ retrieve_titles_urls_from_websites.py
@@ -6,22 +6,22 @@ def retrieve_from_hri(driver):
     pdfurllist =  []
     pdfnamelist = []
 
-    elementllist =  driver.find_elements_by_class_name('toc__section')[2:-2]
+    elementllist =  driver.find_elements_by_class_name('toc__section')
     for i, section in enumerate(elementllist):
-        #print(section.get_attribute('outerHTML'))
         try:
           section.find_element_by_partial_link_text('SESSION').click()
+          print(section.find_element_by_partial_link_text('SESSION').get_attribute('textContent'))
         except NoSuchElementException:  
           pass
         except ElementClickInterceptedException:
           pass
           
-        time.sleep(3)
+        time.sleep(10)
         for j, paper_element in enumerate(section.find_elements_by_class_name('issue-item__content')):
           
           try:
-            paper_name = paper_element.find_element_by_xpath('div/h5').text
-           
+            paper_name = paper_element.find_element_by_xpath('div/h5').get_attribute('textContent')
+            #print(paper_name)
             pdf_url = paper_element.find_element_by_class_name("red").get_attribute('href')
             print(paper_name,pdf_url)
             pdfnamelist.append(paper_name)
